0010456df9347a41715f2ff20134e3d5ce42411f
BridgerB <git@bridgerb.com>
2026-01-27 22:46:09 -0700

    feat: improve hay time

--- Save0/mcp_sim.py
+++ Save0/mcp_sim.py
@@ -17,8 +17,7 @@ def traverse_column_down(height):
 		move(South)
 	farm_cell()
 
-def farm_zone(x_start, x_end, y_start, y_end):
-	nav.go_to(x_start, y_start)
+def farm_zone_once(x_start, x_end, y_start, y_end):
 	height = y_end - y_start - 1
 	for col in range(x_start, x_end):
 		if (col - x_start) % 2 == 0:
@@ -28,14 +27,23 @@ def farm_zone(x_start, x_end, y_start, y_end):
 		if col < x_end - 1:
 			move(East)
 
+def farm_zone_loop(x_start, x_end, y_start, y_end):
+	nav.go_to(x_start, y_start)
+	while num_items(Items.Hay) < 10000000:
+		farm_zone_once(x_start, x_end, y_start, y_end)
+		nav.go_to(x_start, y_start)
+
 def make_worker(x_start, x_end, y_start, y_end):
 	def worker():
-		farm_zone(x_start, x_end, y_start, y_end)
+		farm_zone_loop(x_start, x_end, y_start, y_end)
 	return worker
 
 start = get_time()
-while num_items(Items.Hay) < 10000000:
-	drone.run_parallel(make_worker, farm_zone)
+zones = drone.get_zone_bounds()
+for i in range(1, len(zones)):
+	z = zones[i]
+	spawn_drone(make_worker(z[0], z[1], z[2], z[3]))
+farm_zone_loop(zones[0][0], zones[0][1], zones[0][2], zones[0][3])
 elapsed = get_time() - start
 quick_print("Hay: " + str(num_items(Items.Hay)))
 quick_print("Time to 10M: " + str(elapsed) + "s")
