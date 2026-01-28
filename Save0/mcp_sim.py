4b359e069979732c3ce05ca34d4563dae5da2fb9
BridgerB <git@bridgerb.com>
2026-01-27 22:37:00 -0700

    feat: improve hay sim

--- Save0/mcp_sim.py
+++ Save0/mcp_sim.py
@@ -1,20 +1,23 @@
-def farm_cell():
+import nav
+import drone
+
+def farm_cell(x, y):
 	if can_harvest():
 		harvest()
 	if get_entity_type() == None:
 		plant(Entities.Grass)
 
-def cycle():
-	size = get_world_size()
-	for x in range(size):
-		for y in range(size):
-			move(North)
-			farm_cell()
-		move(East)
+def farm_zone(x_start, x_end, y_start, y_end):
+	nav.traverse_zone(x_start, x_end, y_start, y_end, farm_cell)
+
+def make_worker(x_start, x_end, y_start, y_end):
+	def worker():
+		farm_zone(x_start, x_end, y_start, y_end)
+	return worker
 
 start = get_time()
 while num_items(Items.Hay) < 1000000:
-	cycle()
+	drone.run_parallel(make_worker, farm_zone)
 elapsed = get_time() - start
 quick_print("Hay: " + str(num_items(Items.Hay)))
 quick_print("Time to 1M: " + str(elapsed) + "s")
