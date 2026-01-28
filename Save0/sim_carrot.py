586141665fd05e8ce1604bfad55c474582817a91
BridgerB <git@bridgerb.com>
2026-01-28 00:31:10 -0700

    feat: improved carrot sim

--- Save0/sim_carrot.py
+++ Save0/sim_carrot.py
@@ -14,7 +14,7 @@ def plant_checkerboard():
 			use_item(Items.Water)
 			plant(Entities.Carrot)
 		else:
-			plant(Entities.Tree)
+			plant(Entities.Bush)
 		move(North)
 
 for col in range(size - 1):
@@ -25,8 +25,6 @@ plant_checkerboard()
 while num_drones() > 1:
 	pass
 
-quick_print("Checkerboard planted, waiting...")
-
 while get_pos_x() != 0:
 	move(West)
 while get_pos_y() != 0:
@@ -35,27 +33,22 @@ while get_pos_y() != 0:
 while not can_harvest():
 	pass
 
-def harvest_carrots():
+def harvest_loop():
 	x = get_pos_x()
-	for row in range(size):
+	while num_items(Items.Carrot) < TARGET:
 		y = get_pos_y()
 		if (x + y) % 2 == 0:
 			harvest()
-			use_item(Items.Water)
 			plant(Entities.Carrot)
 		move(North)
 
-quick_print("Starting harvest loop...")
+for col in range(size - 1):
+	spawn_drone(harvest_loop)
+	move(East)
+harvest_loop()
 
-while num_items(Items.Carrot) < TARGET:
-	for col in range(size - 1):
-		spawn_drone(harvest_carrots)
-		move(East)
-	harvest_carrots()
-	while num_drones() > 1:
-		pass
-	while get_pos_x() != 0:
-		move(West)
+while num_drones() > 1:
+	pass
 
 quick_print("Carrots: " + str(num_items(Items.Carrot)))
 quick_print("Time: " + str(get_time() - start) + "s")
