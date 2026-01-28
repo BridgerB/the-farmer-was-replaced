305121a3d0e4d3a7c3c658333fe993378a136e59
BridgerB <git@bridgerb.com>
2026-01-27 23:42:21 -0700

    feat: improved wood sim

--- Save0/sim_wood.py
+++ Save0/sim_wood.py
@@ -1,14 +1,19 @@
 TARGET = 10000000
-
 clear()
 
-def worker():
-	while num_items(Items.Wood) < TARGET:
-		entity = get_entity_type()
-		if entity == Entities.Tree or entity == Entities.Bush:
-			if can_harvest():
-				harvest()
+def planter():
+	for i in range(32):
+		x = get_pos_x()
+		y = get_pos_y()
+		if (x + y) % 2 == 0:
+			plant(Entities.Tree)
 		else:
+			plant(Entities.Bush)
+		move(North)
+
+def harvester():
+	while num_items(Items.Wood) < TARGET:
+		if can_harvest():
 			harvest()
 			x = get_pos_x()
 			y = get_pos_y()
@@ -20,17 +25,31 @@ def worker():
 
 start = get_time()
 size = get_world_size()
-spawn_drone(worker)
+
+spawn_drone(planter)
 for c in range(size - 2):
 	move(East)
-	spawn_drone(worker)
+	spawn_drone(planter)
 move(East)
-while num_items(Items.Wood) < TARGET:
-	entity = get_entity_type()
-	if entity == Entities.Tree or entity == Entities.Bush:
-		if can_harvest():
-			harvest()
+for i in range(32):
+	x = get_pos_x()
+	y = get_pos_y()
+	if (x + y) % 2 == 0:
+		plant(Entities.Tree)
 	else:
+		plant(Entities.Bush)
+	move(North)
+
+while num_drones() > 1:
+	pass
+
+for c in range(size - 1):
+	move(West)
+	spawn_drone(harvester)
+spawn_drone(harvester)
+
+while num_items(Items.Wood) < TARGET:
+	if can_harvest():
 		harvest()
 		x = get_pos_x()
 		y = get_pos_y()
@@ -39,5 +58,6 @@ while num_items(Items.Wood) < TARGET:
 		else:
 			plant(Entities.Bush)
 	move(North)
+
 quick_print("Wood: " + str(num_items(Items.Wood)))
 quick_print("Time to 10M: " + str(get_time() - start) + "s")
