4d32400f06f12cbaccb6470790866f0c224b72c1
BridgerB <git@bridgerb.com>
2026-01-28 00:27:13 -0700

    feat: improved carrot sim

--- Save0/sim_carrot.py
+++ Save0/sim_carrot.py
@@ -2,49 +2,60 @@ TARGET = 10000000
 
 clear()
 
-def tiller_planter():
-	for i in range(32):
-		till()
-		use_item(Items.Water)
-		plant(Entities.Carrot)
-		move(North)
-
-def harvester():
-	while num_items(Items.Carrot) < TARGET:
-		harvest()
-		if get_water() < 0.3:
-			use_item(Items.Water)
-		plant(Entities.Carrot)
-		move(North)
-
 start = get_time()
 size = get_world_size()
 
-spawn_drone(tiller_planter)
-for c in range(size - 2):
+def plant_checkerboard():
+	x = get_pos_x()
+	for row in range(size):
+		y = get_pos_y()
+		if (x + y) % 2 == 0:
+			till()
+			use_item(Items.Water)
+			plant(Entities.Carrot)
+		else:
+			plant(Entities.Tree)
+		move(North)
+
+for col in range(size - 1):
+	spawn_drone(plant_checkerboard)
 	move(East)
-	spawn_drone(tiller_planter)
-move(East)
-for i in range(32):
-	till()
-	use_item(Items.Water)
-	plant(Entities.Carrot)
-	move(North)
+plant_checkerboard()
 
 while num_drones() > 1:
 	pass
 
-for c in range(size - 1):
+quick_print("Checkerboard planted, waiting...")
+
+while get_pos_x() != 0:
 	move(West)
-	spawn_drone(harvester)
-spawn_drone(harvester)
+while get_pos_y() != 0:
+	move(South)
+
+while not can_harvest():
+	pass
+
+def harvest_carrots():
+	x = get_pos_x()
+	for row in range(size):
+		y = get_pos_y()
+		if (x + y) % 2 == 0:
+			harvest()
+			use_item(Items.Water)
+			plant(Entities.Carrot)
+		move(North)
+
+quick_print("Starting harvest loop...")
 
 while num_items(Items.Carrot) < TARGET:
-	harvest()
-	if get_water() < 0.3:
-		use_item(Items.Water)
-	plant(Entities.Carrot)
-	move(North)
+	for col in range(size - 1):
+		spawn_drone(harvest_carrots)
+		move(East)
+	harvest_carrots()
+	while num_drones() > 1:
+		pass
+	while get_pos_x() != 0:
+		move(West)
 
-quick_print("Carrot: " + str(num_items(Items.Carrot)))
-quick_print("Time to 10M: " + str(get_time() - start) + "s")
+quick_print("Carrots: " + str(num_items(Items.Carrot)))
+quick_print("Time: " + str(get_time() - start) + "s")
